ae59c46f26a847009225ed7e6bbc3bc822359f4f
Martin Laws <martinblaws@gmail.com>
2019-06-21 14:51:43 -0400

    Implemented Google Analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -65,6 +65,12 @@ module.exports = {
         cacheTime: 600000, // default
       }
     },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-142544893-1'
+      }
+    }
   ],
   transformers: {
     remark: {
