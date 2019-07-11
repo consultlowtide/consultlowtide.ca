2b69250695883c0a713c4c48d3fc16573d23926e
Martin Laws <mlaws@globeandmail.com>
2019-07-11 12:51:26 -0400

    Added hidden form input with form name

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -69,6 +69,8 @@
 
         <div class="text-lg sm:text-lg mb-16">
           <form name="contact" method="POST" class="mb-12" netlify>
+            <input type="hidden" name="form-name" value="contact" />
+
             <div class="flex flex-wrap mb-6 -mx-4">
                 <div class="w-full md:w-1/2 mb-6 md:mb-0 px-4">
                     <label class="block mb-2 text-copy-primary" for="name">
