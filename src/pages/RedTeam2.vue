0df0c3788a8820eb6acb9724e9cdb7ad881a9fdb
Martin Laws <martinblaws@gmail.com>
2020-02-18 17:05:22 -0500

    Updated lint rules and added RedTeam sample mocks

---
+++ src/pages/RedTeam2.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <g-image
+      class="fw-image-option"
+      src="../../static/red-team/option2.jpg"
+      alt="red team option 2"
+    />
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Martin Laws"
+  }
+};
+</script>
+
+<style scoped>
+.fw-image-option {
+  width: 100vw;
+}
+</style>
---
+++ src/pages/RedTeam3.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <g-image
+      class="fw-image-option"
+      src="../../static/red-team/option3.jpg"
+      alt="red team option 3"
+    />
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Martin Laws"
+  }
+};
+</script>
+
+<style scoped>
+.fw-image-option {
+  width: 100vw;
+}
+</style>
